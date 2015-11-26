1fef9bb4d862fe4aacb576ba9362a83ca9eb2424
Tom Cornebize <tom.cornebize@ensimag.grenoble-inp.fr>
2015-11-26 11:27:16 +0100

    Commence l'implémentation en C++.

---
+++ computepatch_c++/patchAtom.cpp
@@ -0,0 +1,65 @@
+#include "patch.hpp"
+
+PatchAtom::PatchAtom(int line_nb) {
+    this->line_nb = line_nb ;
+}
+
+IdentityAtom::IdentityAtom(int line_nb) : PatchAtom(line_nb) {
+
+}
+
+int IdentityAtom::compute_cost(void) const {
+    return 0 ;
+}
+
+void IdentityAtom::printInStream(std::ostream stream) const {
+
+}
+
+AdditionAtom::AdditionAtom(int line_nb, const std::string &new_line) : PatchAtom(line_nb) {
+    this->new_line = new_line ;
+}
+
+int AdditionAtom::compute_cost(void) const {
+    return 10 + new_line.size() ;
+}
+
+void AdditionAtom::printInStream(std::ostream stream) const {
+    stream << "+" << this->line_nb << "\n" << this->new_line << "\n" ;
+}
+
+SubstituteAtom::SubstituteAtom(int line_nb, const std::string &subs_line) : PatchAtom(line_nb) {
+    this->subs_line = subs_line ;
+}
+
+int SubstituteAtom::compute_cost(void) const {
+    return 10 + subs_line.size() ;
+}
+
+void SubstituteAtom::printInStream(std::ostream stream) const {
+    stream << "=" << this->line_nb << "\n" << this->subs_line << "\n" ;
+}
+
+DestructionAtom::DestructionAtom(int line_nb) : PatchAtom(line_nb) {
+
+}
+
+int DestructionAtom::compute_cost(void) const {
+    return 10 ;
+}
+
+void DestructionAtom::printInStream(std::ostream stream) const {
+    stream << "d" << this->line_nb << "\n" ;
+}
+
+DestructionMultAtom::DestructionMultAtom(int line_nb, int size) : PatchAtom(line_nb) {
+    this->size = size ;
+}
+
+int DestructionMultAtom::compute_cost(void) const {
+    return 15 ;
+}
+
+void DestructionMultAtom::printInStream(std::ostream stream) const {
+    stream << "D" << this->line_nb << " " << this->size << "\n" ;
+}
---
+++ computepatch_c++/patchAtom.hpp
@@ -0,0 +1,56 @@
+#include <string>
+#include <iostream>
+
+class PatchAtom {
+protected:
+    int line_nb ;
+
+public:
+    PatchAtom(int line_nb) ;
+    virtual int compute_cost(void) const = 0 ;
+    virtual void printInStream(std::ostream stream) const = 0 ;
+};
+
+class IdentityAtom : public PatchAtom {
+public:
+    IdentityAtom(int line_nb) ;
+    int compute_cost(void) const = 0 ;
+    void printInStream(std::ostream stream) const ;
+};
+
+class AdditionAtom : public PatchAtom {
+private:
+    std::string new_line ;
+
+public:
+    AdditionAtom(int line_nb, const std::string &new_line) ;
+    int compute_cost(void) const = 0 ;
+    void printInStream(std::ostream stream) const ;
+};
+
+class SubstituteAtom : public PatchAtom {
+private:
+    std::string subs_line ;
+
+public:
+    SubstituteAtom(int line_nb, const std::string &subs_line) ;
+    int compute_cost(void) const = 0 ;
+    void printInStream(std::ostream stream) const ;
+};
+
+class DestructionAtom : public PatchAtom {
+public:
+    DestructionAtom(int line_nb) ;
+    int compute_cost(void) const = 0 ;
+    void printInStream(std::ostream stream) const ;
+};
+
+class DestructionMultAtom : public PatchAtom {
+private:
+    int size ;
+
+public:
+    DestructionMultAtom(int line_nb, int size) ;
+    int compute_cost(void) const = 0 ;
+    void printInStream(std::ostream stream) const ;
+};
